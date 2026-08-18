5d55ae47004d870b7200319255d9f48f005eccf7
Christoper Patiño <christopher.code.git@gmail.com>
2026-08-18 11:05:48 -0600

    Refactor welcome

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -1,9 +1,9 @@
 import { Head } from '@inertiajs/react';
+import { Footer } from '@/partials/footer';
 import { Header } from '@/partials/header';
 import { Hero } from '@/partials/home/hero';
 import { LoanPrograms } from '@/partials/home/loan-programs';
 import { StatsMarquee } from '@/partials/home/stats-marquee';
-import { Footer } from '@/partials/footer';
 
 export default function Welcome() {
     return (
